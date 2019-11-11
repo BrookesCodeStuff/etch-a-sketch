7e8e3941552408de3d26b646b6d7b1d1b7ccee20
Brooke <brookepaglia@gmail.com>
2019-11-11 12:28:00 -0500

    Reset grid and color buttons working

--- main.js
+++ main.js
@@ -2,9 +2,8 @@ let gridCells;
 const gridParent = document.querySelector('.grid-parent');
 
 function buildGrid(num) {
-  gridCells = num;
-  dblNum = num * num;
-  gridParent.setAttribute("style", "grid-template-columns: repeat(" + `${gridCells}` + ", 1fr); grid-template-rows: repeat(" + `${gridCells}` + ", 1fr");
+  let dblNum = num * num;
+  gridParent.setAttribute("style", "grid-template-columns: repeat(" + `${num}` + ", 1fr); grid-template-rows: repeat(" + `${num}` + ", 1fr");
   for (i = 1; i <= dblNum; i++) {
     let newDiv = document.createElement('div');
     newDiv.setAttribute('id', i);
@@ -15,6 +14,24 @@ function buildGrid(num) {
 }
 
 buildGrid(50);
+
+gridCells = document.querySelectorAll(".cell");
+
+function reset() {
+  let num = prompt("From 4 to 100, how many grid squares is the new grid?");
+  gridCells.forEach(cell => gridParent.removeChild(cell));
+  buildGrid(num);
+}
+
+function setRandBg() {
+  gridCells = document.querySelectorAll(".cell");
+  gridCells.forEach(cell => cell.setAttribute('onmouseover', 'randBg()'));
+}
+
+function setBlackBg() {
+  gridCells = document.querySelectorAll(".cell");
+  gridCells.forEach(cell => cell.setAttribute('onmouseover', 'blackBg()'));
+}
 
 function random_rgba() {
   var o = Math.round, r = Math.random, s = 255;
